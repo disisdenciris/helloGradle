2d86e2e68cf734a45487a764adb387315cff989b
disisdenciris <huseyin.bacanak@tpe.gov.tr>
2014-07-21 11:51:54 +0300

    first commit

---
+++ src/main/java/com/hb/hellogradle/Main.java
@@ -0,0 +1,10 @@
+package com.hb.hellogradle;
+
+/**
+ * Created by huseyin.bacanak on 21.07.2014.
+ */
+public class Main {
+    public static void main (String... args){
+        System.out.println("step 1 complete");
+    }
+}
